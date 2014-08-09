feb91452d2b0e79a9a4edd34927a1b329ac0d9ee
rcadaoas <romeo.mail7@gmail.com>
2014-08-09 18:23:05 +0400

    8. Updating a Bear’s Info PUT /api/bears/:bear_id

--- RESTful API/server.js
+++ RESTful API/server.js
@@ -65,12 +65,30 @@ router.route('/bears')
 // on routes that end in /bears/:bear_id
 //-----------------------------------------------------
 router.route('/bears/:bear_id')
+
 	//get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
 	.get(function(req,res){
 		Bear.findById(req.params.bear_id,function(err,bear){
 			if(err)
 				res.send(err);
 			res.json(bear);
+		});
+	})
+
+	//update the bear with thi id(accessed at put http://localhost:8080/api/bears/:bear_id)
+	.put(function(req,res){
+		Bear.findById(req.params.bear_id, function(res,bear){
+			if(err)
+				res.send(err);
+
+			bear.name=req.body.name; // update the bear name
+
+			//save the bear
+			bear.save(function(err){
+				if(err)
+					res.send(err);
+				res.json({message: 'Bear updated'});
+			});
 		});
 	});
 
