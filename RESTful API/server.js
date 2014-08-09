2c374815bb5ec54e22d7aeee83261f53c6cbb024
rcadaoas <romeo.mail7@gmail.com>
2014-08-09 17:54:06 +0400

    5. Creating a Bear POST /api/bears

--- RESTful API/server.js
+++ RESTful API/server.js
@@ -34,6 +34,27 @@ router.get('/', function(req, res){
 
 //more routes for our API will happen here
 
+//on routes that end in /bears
+//-----------------------------------------------------
+router.route('/bears')
+
+	//create a bear(accessed at POST http://localhost:8080/api/bears)
+	.post(function(req,res){
+		var bear = new Bear(); //create a new instance of Bear Model
+		bear.name=req.body.name; //set the bears name (comes from the request)
+
+		//save the bear and check for errors
+		bear.save(function(err){
+			if(err)
+				res.send(err);
+
+			res.json({message: 'Bear created!'});
+		});
+
+	});
+
+	
+
 //REGISTER OUR ROUTES----------------------------------
 //all of our routes will be prefixed with /api
 app.use('/api',router);
