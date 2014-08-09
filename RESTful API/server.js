f403f535860a71f7d7720cea855350c495698974
rcadaoas <romeo.mail7@gmail.com>
2014-08-09 17:21:16 +0400

    3. Creating Our Database and Connecting/Bear Model app/models/bear.js

--- RESTful API/server.js
+++ RESTful API/server.js
@@ -5,6 +5,9 @@
 var express = require('express');		//call express
 var app = express();					//define our app using express
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://startup:password@proximus.modulusmongo.net:27017/Q2imuqeb'); // connect to our database
 
 //configure app to use bodyParser()
 //this will let us get the data from POST
